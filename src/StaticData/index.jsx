5959d18c9e3a23dc0f278bc7463387ba84b4e9c9
surendrasuresh1999 <k.surendraparla1999@gmail.com>
2024-05-27 18:38:43 +0530

    removed unwanted code

--- src/StaticData/index.jsx
+++ src/StaticData/index.jsx
@@ -11,58 +11,6 @@ import knowledge from "../assets/knowledge-base.png";
 import motivation from "../assets/motivation (1).png";
 import Other from "../assets/other.png";
 
-export const indianStatesArray = [
-  "Andaman and Nicobar Islands",
-  "Andhra Pradesh",
-  "Arunachal Pradesh",
-  "Assam",
-  "Bihar",
-  "Chandigarh",
-  "Chhattisgarh",
-  "Dadra and Nagar Haveli",
-  "Daman and Diu",
-  "Delhi",
-  "Goa",
-  "Gujarat",
-  "Haryana",
-  "Himachal Pradesh",
-  "Jammu and Kashmir",
-  "Jharkhand",
-  "Karnataka",
-  "Kerala",
-  "Ladakh",
-  "Lakshadweep",
-  "Madhya Pradesh",
-  "Maharashtra",
-  "Manipur",
-  "Meghalaya",
-  "Mizoram",
-  "Nagaland",
-  "Odisha",
-  "Puducherry",
-  "Punjab",
-  "Rajasthan",
-  "Sikkim",
-  "Tamil Nadu",
-  "Telangana",
-  "Tripura",
-  "Uttar Pradesh",
-  "Uttarakhand",
-  "West Bengal",
-];
-
-export const sizesObj = [
-  "XXS",
-  "XS",
-  "S",
-  "M",
-  "LG",
-  "XL",
-  "XXL",
-  "2XL",
-  "3XL",
-];
-
 export const categoryOptions = [
   {
     id: 0,
